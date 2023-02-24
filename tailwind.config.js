55cb472c326ca7c051a2c986ccb7a3d7978108e9
Christian Delicaat <c.delicaat@hotmail.com>
2023-02-24 12:42:49 +0100

    Installed Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.twig",
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
